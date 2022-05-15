5250fd70e82c7b0ee0bb35d73def1d77ea7f3b53
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-15 18:41:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
-
 # ID     Name   Age
 # field1\tfield2\tfield3\n
 # field1\tfield2\tfield3\n
 
-from student import student
-from teacher import teacher
-from admin import admin
-
-def main():
-    choice = 1
-    while choice != 0:
-        print('1- Student')
-        print('2- Teacher')
-        print('3- Admin')
-        print('0- Exist\n')
-        choice = int(input('Choice: '))
-        if choice == 1:
-            student()
-        elif choice == 2:
-            teacher()
-        elif choice == 3:
-            admin()
-        elif choice != 0:
-            # choice not in [0 ... 3]
-            print('Choose a number between 0 and 3')
-
-    print('--> Goodbye <--')
-
-if __name__ == "__main__":
-    main()
-
-    
 from student import student
 from teacher import teacher
 from admin import admin
