b6722539822d1b5353c62b74f3904b947e6aa37c
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-15 22:36:26 +0200

    Update help.py
    v1.0

--- help.py
+++ help.py
@@ -4,6 +4,11 @@ import os
 import time
 from msvcrt import getch        # for windows users
 
+
+# record of Student ->     Id + '\t' + Name + '\t' + Age + '\t' + Department + '\t' + Level + '\t' + Password + '\t' + [courses] + '\t\n'
+# record of Teahcer ->     Id + '\t' + Name + '\t' + Age + '\t' + Course_name + '\t' + Password + '\t' + [students] + '\t\n'
+
+
 students_ids = PriorityQueue()
 teachers_ids = PriorityQueue()
 
@@ -27,22 +32,22 @@ def Returning():
     for i in range(3):
         print('.', end='')
         time.sleep(0.5)
-    os.system("cls");
+    os.system("cls")
 
 def init_student_ids():
     with open("Student.txt", 'r') as student_file:
         for record in student_file:
             fields = record.split('\t')
-            existance_student_id[int(fields[0])] = 1;
+            existance_student_id[int(fields[0])] = 1
         for i in range(1, 205, 1):
             if not existance_student_id[i]:
-                students_ids.put(i);
+                students_ids.put(i)
 
 def init_teacher_ids():
     with open("Teachers.txt", 'r') as teacher_file:
         for record in teacher_file:
             fields = record.split('\t')
-            existance_teacher_id[int(fields[0])] = 1;
+            existance_teacher_id[int(fields[0])] = 1
         for i in range(1, 205, 1):
             if not existance_teacher_id[i]:
-                teachers_ids.put(i);
+                teachers_ids.put(i)
