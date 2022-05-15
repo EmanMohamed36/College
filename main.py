2bf32a59c1195cbf140423ecfe425c9925688e38
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-15 18:41:19 +0200

    Update main.py
    import help
    init ids
    directing 
    redirecting

--- main.py
+++ main.py
@@ -29,3 +29,37 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    
+from student import student
+from teacher import teacher
+from admin import admin
+from help import *
+
+def main():
+    init_student_ids()
+    init_teacher_ids()
+    choice = 1
+    while choice != 0:
+        print('1- Student')
+        print('2- Teacher')
+        print('3- Admin')
+        print('0- Exist\n')
+        choice = int(input('Choice: '))
+        if choice == 1:
+            student()
+            Directing()
+        elif choice == 2:
+            teacher()
+            Directing()
+        elif choice == 3:
+            admin()
+            Directing()
+        elif choice != 0:
+            print('Choose a number between 0 and 3')
+            Directing()
+    Returning()
+    print('--> Goodbye <--')
+
+if __name__ == "__main__":
+    main()
