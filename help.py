4297452f8c23cb2d0997d04b4e5f92c8ce940e61
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-18 22:14:43 +0200

    Update help.py

--- help.py
+++ help.py
@@ -4,6 +4,29 @@ import os
 import time
 from msvcrt import getch        # for windows users
 
+class bcolors:
+    red = '\033[38;5;196m'
+    green = '\033[38;5;40m'
+    blue = '\033[34m'
+    gold = '\033[38;5;220m'
+    white = '\33[37m'
+    magenta = '\033[35m'
+    brown = '\033[38;5;94m'
+    bold = '\033[01m'
+    reset = '\033[0m'
+
+def SetCo(s):
+    lis = {
+        bcolors.red: 'red', bcolors.green: 'green', 
+        bcolors.blue:'blue', bcolors.gold: 'gold',
+        bcolors.magenta : 'magenta', bcolors.brown : 'brown', 
+        bcolors.reset : 'reset', bcolors.white: 'white',
+    }
+    lis = { value:key for key, value in lis.items()}
+    print(f'{lis[s]}', end='')
+
+def PutSp(n, ch):
+    print(n * str(ch), end='')
 
 # record of Student ->     Id + '\t' + Name + '\t' + Age + '\t' + Department + '\t' + Level + '\t' + Password + '\t' + [courses] + '\t\n'
 # record of Teahcer ->     Id + '\t' + Name + '\t' + Age + '\t' + Course_name + '\t' + Password + '\t' + [students] + '\t\n'
@@ -20,6 +43,7 @@ def press_any():
     print("Press any key to return back...", end = '', flush=True)
     getch()
     print()
+    
 
 def Directing():
     print('Redicrecting', end='', flush = True)
