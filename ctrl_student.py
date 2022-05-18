de65527dfefdf6444d03df37ea793362e6406016
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-18 22:32:18 +0200

    Update ctrl_student.py

--- ctrl_student.py
+++ ctrl_student.py
@@ -8,38 +8,132 @@ def searchStudent():
         for line in file:
             fields = line.split('\t')
             if stu_id == fields[0]:
-                print(line,end = '')
+                print('ID\tName\tAge\tDepartment\tLevel\tPassword')
+                print('------------------------------------------')
+                for i in range(6):
+                    print(fields[i] + '\t', end='')
+                print('\n')
                 flag = 1
         if not flag:
             print('Not Found')
+    press_any()
+    Returning()
+
+
+def Remove_Teacher():
+    Id = input('Enter The id To delete: ')
+    flag = False
+    with open('Teachers.txt', 'r') as TecFile, open('Temp.txt', 'w') as temporary_file:
+        for record in TecFile:
+            field = record.split('\t')
+            if field[0] == Id:
+                flag = True
+            else:
+                temporary_file.write(record)
+                
+    os.remove('Teachers.txt')
+    os.rename('Temp.txt' , 'Teachers.txt')
+
+    if not flag:
+        print('teacher Not Found')
+        press_any()
+        Returning()
+    
+    teachers_ids.put(int(Id))
+    print('deleted successfully')
+    with open('Student.txt', 'r') as student_file, open('Temporary.txt', 'w') as Tempo:
+        for record in student_file:
+            fields = record.split('\t')
+            fields.pop()
+            if Id in fields[6:]:
+                fields = [fields[i] for i in range(0, len(fields)) if i < 6 or fields[i] != Id]
+                record = ""
+                for i in range(len(fields)):
+                    record += fields[i] + '\t'
+                record += '\n'
+            Tempo.write(record)
+            
+    os.remove('Student.txt')
+    os.rename('Temporary.txt', 'Student.txt')
+    press_any()
+    Returning()
 
 def delete_student():
-    student_id = input('Enter the id of the student to delete: ')
+    Id = input('Enter The id To delete: ')
+    flag = False
+    with open('Student.txt', 'r') as student_file, open('Temp.txt', 'w') as temp_file:
+        for record in student_file:
+            field = record.split('\t')
+            if field[0] == Id:
+                flag = True
+            else:
+                temp_file.write(record)
+                
+    os.remove('Student.txt')
+    os.rename('Temp.txt' , 'Student.txt')
+    
+    if not flag:
+        print('Student Not Found')
+        press_any()
+        Returning()
+        return
+
+    students_ids.put(int(Id))
+    print('deleted successfully')
+    with open('Teachers.txt', 'r') as teacher_file, open('Temporary.txt', 'w') as Tempo:
+        for record in teacher_file:
+            fields = record.split('\t')
+            fields.pop()
+            if Id in fields[5:]:
+                fields = [fields[i] for i in range(0, len(fields)) if i < 5 or fields[i] != Id]
+                record = ""
+                for i in range(len(fields)):
+                    record += fields[i] + '\t'
+                record += '\n'
+            Tempo.write(record)
+            
+    os.remove('Teachers.txt')
+    os.rename('Temporary.txt', 'Teachers.txt')
+    press_any()
+    Returning()
+
+def update_student(_id = '-1'):
+    _id = input('Enter The id To delete: ')
     with open('Student.txt', 'r') as student_file, open('Temp.txt', 'w') as temp_file:
         flag = False
         for record in student_file:
             fields = record.split('\t')
-            if student_id == fields[0]:
+            fields.pop()
+            if _id == fields[0]:
                 flag = True    
-            else:
-                temp_file.write(record)
+                
+                ch = input('Do you wnat to change the Name (Y / N)? ')
+                if ch == 'y' or ch == 'Y':
+                    fields[1] = input('\tEnter the new name: ')
+
+                ch = input('Do you wnat to change the Age (Y / N)? ')
+                if ch == 'y' or ch == 'Y':
+                    fields[2] = input('\tEnter the new Age: ')
+
+                ch = input('Do you wnat to change the Department (Y / N)? ')
+                if ch == 'y' or ch == 'Y':
+                    fields[3] = input('\tEnter the new Department: ')
+                     
+                record = ""
+                for i in range(len(fields)):
+                    record += fields[i] + '\t'
+                record += '\n'
+            temp_file.write(record)
+
         if not flag:
             print('Student not found')
         else:
-            print('Student deleted successfully')
+            print('Student updated successfully')
+
     os.remove('Student.txt')
     os.rename('Temp.txt', 'Student.txt')
-    if flag:
-        with open('Teachers.txt', 'r') as teacher_file, open('Temporary.txt', 'w') as temporary_file:
-            flag = False
-            for record in teacher_file:
-                fields = record.split('\t')
-                if student_id not in fields[5:]:
-                    temporary_file.write(record)
-
-        os.remove('Teachers.txt')
-        os.rename('Temporary.txt', 'Teachers.txt')
-
+    press_any()
+    Returning()
 
 def ctrl_student():
     while 1:
@@ -54,24 +148,24 @@ def ctrl_student():
         Ad_Stud_choice = input('Choise: ')
         if Ad_Stud_choice == '1':
             #show all student
-            read_student()
             Directing()
+            read_student()
         elif Ad_Stud_choice == '2':
             #search student
-            searchStudent()
             Directing()
+            searchStudent()
         elif Ad_Stud_choice == '3':
             #Add
-            write_student()
             Directing()
+            write_student()
         elif Ad_Stud_choice == '4':
             #remove
-            delete_student()
             Directing()
+            delete_student()
         elif Ad_Stud_choice == '5':
             #Edit
-            update_student()
             Directing()
+            update_student()
         elif Ad_Stud_choice == '6':
             #return
             Returning()
