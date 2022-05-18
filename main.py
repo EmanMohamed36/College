e34682627a4030bb236a37bde5c7d5090cedf576
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-18 22:11:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from teacher import teacher
 from admin import admin
 from help import *
 
+# def prints(srtn):
+    
+#PutSp(num, 'ch')
+#SetCo('color')
+
 def main():
     init_student_ids()
     init_teacher_ids()
     choice = 1
     while choice != 0:
+
+        print('Main Menu\n')
         print('1- Student')
         print('2- Teacher')
         print('3- Admin')
         print('0- Exist\n')
+        
+
         choice = int(input('Choice: '))
         if choice == 1:
             student()
